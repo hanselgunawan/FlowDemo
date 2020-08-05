70bce1da373a05035d96670baf7e1135a63bbf5e
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-08-04 19:45:32 -0700

    added zip operator

--- app/src/main/java/com/hanseltritama/flowdemo/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/flowdemo/MainActivity.kt
@@ -9,7 +9,8 @@ import kotlinx.coroutines.flow.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var flow: Flow<Int>
+//    lateinit var flow: Flow<Int>
+    lateinit var flowStr: Flow<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,16 +22,16 @@ class MainActivity : AppCompatActivity() {
     @ExperimentalCoroutinesApi
     fun setupFlow() {
 
-        // 1. flowOf
+        // ========= 1. flowOf ==========
 //        flow = flowOf(1,3,2,4,6,5).onEach {
 //            Log.d("HANSEL", "Emitting $it")
 //            delay(400)
 //        }.flowOn(Dispatchers.Default)
 
-        // 2. asFlow
+        // ========= 2. asFlow ===========
 //        flow = (1..5).asFlow().onEach { delay(300) }.flowOn(Dispatchers.Default)
 
-        // 3. flow
+        // ========== 3. flow ==========
 //        flow = flow {
 //            Log.d("HANSEL", "Start Flow")
 //            (0..10).forEach {
@@ -43,12 +44,33 @@ class MainActivity : AppCompatActivity() {
 //            it * it
 //        }.flowOn(Dispatchers.Default)
 
-        // 4. channelFlow
-        flow = channelFlow {
-            (0..10).forEach {
-                delay(300)
-                send(it)
-            }
+        // ========== 4. channelFlow ==========
+//        flow = channelFlow {
+//            (0..10).forEach {
+//                delay(300)
+//                send(it)
+//            }
+//        }.flowOn(Dispatchers.Default)
+
+        // ========== 5. Exception Handling ==========
+//        flow = channelFlow {
+//            (0..10).forEach {
+//                delay(300)
+//                check(it != 3)
+//                send(it)
+//            }
+//        }.onCompletion {
+//            Log.d("Hansel", "Complete!")
+//        }.catch { e ->
+//            Log.d("Hansel", "Caught $e")
+//        }.flowOn(Dispatchers.Default)
+
+        // ========== 6. Zip Operator ==========
+        val intFlow = flowOf(1,2,3)
+        val strFlow = flowOf("A","B","C")
+
+        flowStr = intFlow.zip(strFlow) {intValue, stringValue ->
+            "$intValue$stringValue"
         }.flowOn(Dispatchers.Default)
 
 
@@ -57,9 +79,9 @@ class MainActivity : AppCompatActivity() {
     private fun setupClicks() {
         button.setOnClickListener {
             CoroutineScope(Dispatchers.Main).launch {
-                flow.collect {
+                flowStr.collect {
                     // print the values one by one
-                    Log.d("HANSEL", it.toString())
+                    Log.d("HANSEL", it)
                 }
             }
         }
